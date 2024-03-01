0336d8cab6a9b792b097e0edb6d97f55c5076aaa
Cadomatsu <cadecollinson64@gmail.com>
2024-02-29 17:16:25 -0700

    Camera follow
    Replaced hacky implementation with another hack.. sad!

---
+++ 1402UTP/Assets/Scripts/CinemachineCameraOffset.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineCameraOffset : MonoBehaviour
+{
+    public SpriteRenderer characterSprite; // My dude
+    public CinemachineVirtualCamera cinemachineCamera;
+    public Vector3 offsetWhenFlipped; // Oh, this is nasty
+    public float smoothTime = 0.2f;
+
+    private CinemachineFramingTransposer transposer;
+    private Vector3 originalOffset;
+
+    void Start()
+    {
+        transposer = cinemachineCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        originalOffset = transposer.m_TrackedObjectOffset;
+    }
+
+    void Update()
+    {
+        // I feel like there has to be a better way to do this within unity..
+        // This tracks the character sprite's flip.x and changes the "Camera Offset" in cinemachine to match.
+        // With a smoothtime lerp to make it feel less jank, as it looked nauseating without it.
+        Vector3 targetOffset = characterSprite.flipX ? offsetWhenFlipped : originalOffset;
+        Vector3 smoothedOffset = Vector3.Lerp(transposer.m_TrackedObjectOffset, targetOffset, smoothTime);
+        transposer.m_TrackedObjectOffset = smoothedOffset;
+    }
+}
